3d4e6c0e599deca2d421693c5d9e0e9b0ae3eb9e
delmaSong <sdy2856@gmail.com>
2022-03-16 22:59:30 +0900

    feat: 나머지가 1이 되는 수 찾기

---
+++ Programmers/나머지가 1이 되는 수 찾기.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var answer = 0
+    for i in 1...(n-1) {
+        if n % i == 1 {
+            answer = i
+            break
+        }
+    }
+
+    return answer
+}
+
+print(solution(12))
